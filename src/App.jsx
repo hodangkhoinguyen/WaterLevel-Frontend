be2ddabc461f0e596a003903e31510aadc9aa71a
hodangkhoinguye <50374959+hodangkhoinguyen@users.noreply.github.com>
2022-05-30 10:29:30 +0000

    Working on Chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 import {sendPostRequest} from './AJAX.jsx';
+import useAsyncFetch from './useAsyncFetch'; 
+import { Bar } from "react-chartjs-2";
+import Chart from 'chart.js/auto';
 
 function App() {
   const [view, updateView] = useState(false);
@@ -51,19 +54,10 @@ function SeeMore(props) {
   let display = props.display;
   console.log(display);
   
-  function getChart() {
-    (async function () {
-      console.log("Doing AJAX request");
-      let date = {"month": "1", "year": "2017"};
-      let newMovies = await sendPostRequest("/query/getChart", date);
-    }) ();
-  }
-  
   if (display) {  
-    useEffect(getChart, []);
     return (
       <div id="seeMore">
-        <div id="chart"></div>
+        <LakeDisplay/>
         <div className="tile">
            <p>
 Here's a quick look at some of the data on reservoirs from the <a href="https://cdec.water.ca.gov/index.html">California Data Exchange Center</a>, which consolidates climate and water data from multiple federal and state government agencies, and  electric utilities.  Select a month and year to see storage levels in the eleven largest in-state reservoirs.
@@ -76,4 +70,108 @@ Here's a quick look at some of the data on reservoirs from the <a href="https://
   }
 }
 
+function LakeChart(props) {
+  let lakes = {
+    "Shasta" : 4552000,
+    "Oroville" : 3537577,
+    "Trinity Lake" : 2447650,
+    "New Melones" : 2400000,
+    "San Luis" : 2041000,
+    "Don Pedro" : 2030000,
+    "Berryessa" : 1602000
+  };
+  
+  const nicknames = new Map();
+  let count = 0;
+  for (let i in lakes) {
+    nicknames.set(count, i);
+    count++;
+  }
+  
+  if (props.lakes) {
+    let n = props.lakes.length;
+    console.log(props.lakes);
+
+
+    let capacity = {label: "capacity", data: [], backgroundColor: []};
+    let storage = {label: "storage", data: [], backgroundColor: []};
+    let labels = [];
+    
+    for (let i=0; i < n; i++) {
+      let lake = nicknames.get(i);
+      capacity.data.push(lake);
+      storage.data.push(props.lakes[i].value);
+      labels.push(lake);
+    }
+  console.log(storage.data);
+    console.log(storage.data);
+  let userData = {};
+  userData.labels = labels;
+  userData.datasets = [capacity, storage];
+
+console.log(userData);
+let options = {
+  plugins: {
+    title: {
+      display: true,
+      text: 'Sticker vs. Middle Income Family Prices',
+    },
+  },
+  responsive: true,
+  maintainAspectRatio: false,
+  scales: {
+    x: {
+      grid: {
+        display: false
+      }
+    },
+    y: {
+      grid: {
+        display: false
+      }
+    }
+  }
+};
+
+
+      return (
+        <div id="chart-container">
+          <Bar options={options} data={userData} />
+        </div>
+      )
+  }
+}
+
+// A component that fetches its own data
+function LakeDisplay() {
+  console.log("in LakeDisplay");
+
+  // static var will contain the list of schools
+  const [lakes, setLake] = useState([]);
+
+  // call the custom fetch hook, passing it the callback functions that it can use
+  useAsyncFetch("/query/getLake", {}, thenFun, catchFun);
+  
+  function thenFun (result) {
+    setLake(lakes);
+    // render the list once we have it
+  }
+
+  function catchFun (error) {
+    console.log(error);
+  }
+
+  // will re-render once state variable schools changes
+  if (lakes) {
+  return (
+    <main>
+      <LakeChart lakes={lakes}> </LakeChart>
+    </main>
+  )
+  } else {
+    return (<p>
+      loading...
+    </p>);
+  }
+}
 export default App;
